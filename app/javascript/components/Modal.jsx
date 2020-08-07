61c3364af1716dab957d85b245deaee9568f04b3
Jared Meier <meier.jared@gmail.com>
2020-08-07 12:34:57 -0400

    Minor styling improvements and add Esc to close modal

--- app/javascript/components/Modal.jsx
+++ app/javascript/components/Modal.jsx
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { GlobalContext } from '../contexts/GlobalContext';
 
 export default (props) => {
   const { showModal, setState } = useContext(GlobalContext);
 
+  const handleCloseModal = () => {
+    // console.log('changing state');
+    setState(prev => ({ ...prev, showModal: false }));
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode !== 27) return;
+      handleCloseModal();
+
+      return document.removeEventListener('keydown', handleCloseModal);
+    });
+  }, []);
+
   if (!showModal) return null;
 
   return (
-    <div className='modal-background'
-      onClick={() => setState(prev => ({...prev, showModal: false}))}
+    <div
+      className='modal-background'
+      onClick={() => setState(prev => ({ ...prev, showModal: false }))}
     >
       <div className='modal-child' onClick={e => e.stopPropagation()}>
         {props.children}
